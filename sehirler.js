01110e783f45d3e69511441a4c1587f80a4e9590
Vedat Sözen <vedatsozen@gmail.com>
2022-06-16 15:11:41 +0300

    Add files via upload

---
+++ sehirler.js
@@ -0,0 +1,50 @@
+
+const sehirler = [
+
+    { 
+        "isim":"Ordu",
+        "bolge":"Karadeniz"
+    },
+    
+    { 
+        "isim":"İstanbul",
+        "bolge":"Marmara"
+    },
+    
+    { 
+        "isim":"Ankara",
+        "bolge":"İç Anadolu"
+    },
+    
+    { 
+        "isim":"Bursa",
+        "bolge":"Ege"
+    },
+    
+    { 
+        "isim":"Kocaeli",
+        "bolge":"Marmara"
+    },
+    
+    { 
+        "isim":"Tokat",
+        "bolge":"Karadeniz"
+    },
+    
+    { 
+        "isim":"Tekirdağ",
+        "bolge":"Marmara"
+    },
+    
+    { 
+        "isim":"İzmir",
+        "bolge":"Ege"
+    },
+    
+    { 
+        "isim":"Kayseri",
+        "bolge":"İç Anadolu"
+    }
+    
+    
+    ]
